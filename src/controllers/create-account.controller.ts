d69dc4a784217194a6b462bdfd3aa664c37b0532
Matheussop <matheussopluiz@outlook.com>
2025-10-25 21:29:40 -0300

    Refactor AppModule to remove unused controllers and providers; add CreateAccountController for account creation functionality; update PrismaService formatting for improved readability.

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Post } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+  @Post()
+  async handle() {}
+}
